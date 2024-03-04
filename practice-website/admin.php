94ce9fa74a64e4697b863d5aacd6ef11c4b05350
XtraStrong <anselalnosnes1989@disroot.org>
2024-03-04 21:41:51 +0800

    Paki-Debug

--- practice-website/admin.php
+++ practice-website/admin.php
@@ -1,22 +1,32 @@
 <?php
 
 include 'database.php';
+session_start();
+if(!isset($_SESSION["user"])){
+    header("Location: login.php");
+}
+
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    if (isset($_POST["movie_id"])) {
+    if (isset($_POST["logout"])){
+        session_start();
+        session_destroy();
+        header("Location:index.php");
+
+     } else if (isset($_POST["movie_id"])) {
         $movieId = $_POST["movie_id"];
 
         $sql = "UPDATE tbl_seats SET seat_status = 'available' WHERE movie_id = ?";
         $stmt = $conn->prepare($sql);
         $stmt->bind_param("i", $movieId);
         if ($stmt->execute()) {
-            echo "Seats status for movie ID $movieId reset successfully.";
+            $message="Seats status for movie ID $movieId reset successfully.";
         } else {
-            echo "Error resetting seats status: " . $conn->error;
+            $message="Error resetting seats status: " . $conn->error;
         }
     } else {
-        echo "Please enter a movie ID.";
+        $message="Please enter a movie ID.";
     }
 }
 ?>
@@ -26,16 +36,54 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <title>Admin - Reset Seats Status</title>
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">    
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        
+
+
+    
 </head>
 <body>
 
-<h1>Admin - Reset Seats Status</h1>
 
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-    <label for="movie_id">Movie ID:</label>
-    <input type="number" id="movie_id" name="movie_id" required>
-    <input type="submit" value="Reset Seats Status">
-</form>
+<div class="container vh-100 d-flex align-items-center">
+    <div class="row justify-content-center">
+        <div class="col-md-7 text-center">
+
+            <?php if(isset($message)): ?>
+            <div class='alert alert-primary my-3 shadow' style='font-size: calc(1.375rem + 0.2vw);'><?php echo $message; ?></div>
+            <?php endif; ?>
+
+            <h1 class='mb-2'>Admin - Reset Seats Status</h1>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <label class="mt-3" for="movie_id" style='font-size: 3vh'>Movie ID:</label>
+                <select id="movie_id" name="movie_id">
+                    <option value="">Select Movie ID</option>
+                    <option value="1">Rewind</option>
+                    <option value="2">Demon Slayer</option>                    
+                </select>
+                <div class='my-3 d-flex justify-content-center'>
+                    <div class='mr-3'> <!-- Add margin to the right of the first button for spacing -->
+                        <input class="btn btn-outline-secondary" type="submit" value="Reset Seats Status">
+                    </div>
+                    <div> <!-- Wrap the logout button in a div to align it next to the reset button -->                            
+                            <button type="submit" class="btn btn-outline-secondary" name="logout">
+                                <i class="fa fa-sign-out" aria-hidden="true"></i> Logout
+                            </button>                        
+                    </div>
+                </div>
+            </form>
+
+        </div>
+    </div>
+</div>
+
+
+
 
 </body>
 </html>
--- practice-website/index.php
+++ practice-website/index.php
@@ -18,6 +18,8 @@ $result = $conn->query($sql)
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    
+    <!-- CSS -->
     <link rel="stylesheet" href=css/index.css>
 
 
@@ -26,41 +28,39 @@ $result = $conn->query($sql)
 
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">                        
         <div class="container">
-            <div id="logo">
-                <a class="navbar-brand" href="#">
-                    <img src="https://www.smsupermalls.com/themes/revamp2022/assets/img/logo.svg" class="d-inline-block align-top" alt="">
-                </a>
-            </div>
-            
-            <a class="navbar-brand" href="#">Movie Queue</a>
+        <div id="logo">
+            <a class="navbar-brand" href="#">
+                <img src="https://www.smsupermalls.com/themes/revamp2022/assets/img/logo.svg" class="d-inline-block align-top" alt="">
+            </a>
+        </div>
+      </div>    
+        <a class="navbar-brand" href="#">Movie Queue</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="about.php">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="contact.php">Contact</a>
-                    </li>
-                </ul>
+                    
+                </ul>                
             </div>
+
+        <!-- Admin Login Page Button -->
+        <button class="btn btn-outline-success btn-lg btn-block" onclick="window.location.href='login.php'">Admin View</button>
+        <!-- Admin Login Page Button -->
+
         </div>
     </nav>
 </header>
     
-<div class="movies-grid">
+<div class="movies-grid justify-content-center my-5 ">
     
 <?php
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
 
-        echo '<div class="movie-container">';
+        echo '<div class="movie-container shadow-lg p-3 mb-5 bg-white rounded ">';
         echo '<img src="movie_poster/' . $row["movie_name"] . '.jpg">';
         echo '<h2 class="movie_title">' . $row["movie_name"] . '</h2>';
         echo '<a href="movie_details.php?id=' .$row["id"] .'">More Details..</a>';
